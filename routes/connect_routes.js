97b8098c4e2e4e10dba362b577d3f50e80256588
Devika0116 <devika.devadas@cavliwireless.com>
2022-11-14 17:51:45 +0530

    hello world api added

--- routes/connect_routes.js
+++ routes/connect_routes.js
@@ -6,5 +6,21 @@ const connectRoute = express.Router();
 connectRoute.route("/").get((req, res) => {
   res.send("Hello World ! ...");
 });
+// const config={username:'BMNeiKxTvuIzonQYAyPacSGz',password:'gSt40WLxW510ZKKMgu7BLwICqSb7hiHiA4iFtD_F7GbGqK3pZPcB.3l0D+E6u0bhqfGIudw-tkqejhAyqJUmAodZq6_NP9MBdFYaLR-jzL-20,ysdqGgXP7xPGInHFEd',contactPoint:'13.112.103.84',localDataCenter:'',keyspace:'',port:9042}
+// let authProvider = new cassandra.auth.PlainTextAuthProvider(
+//     config.username,
+//     config.password
+// );
 
-module.exports = { connectRoute };
+// let client = new cassandra.Client({
+//     contactPoints: [`${config.contactPoint}:${config.port}`],
+//     authProvider: authProvider,
+//     localDataCenter: config.localDataCenter,
+//     keyspace:config.keyspace,
+//     sslOptions: {
+//         secureProtocol: "TLSv1_2_method"
+//     },
+// });
+// client.connect();
+
+module.exports=connectRoute;

a217a485b8f919d1df5d457dedf0c7cde771b43a
bharathpai <bharath.pai@cavliwireless.com>
2022-11-15 12:28:30 +0530

    Local cassandra connect

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 // const cassandra  = require("cassandra-driver");
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var connectRoute = require('./routes/');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var connectRoute = require("./routes/");
 
 // cassandra connection start
-const config={username:'cassandra',password:'cassandra',contactPoint:'13.112.103.84',localDataCenter:'',keyspace:'',port:9042}
+const config = {
+  username: "cassandra",
+  password: "cassandra",
+  contactPoint: "13.112.103.84",
+  localDataCenter: "",
+  keyspace: "",
+  port: 9042
+};
 // let authProvider = new cassandra.auth.PlainTextAuthProvider(
 //     config.username,
 //     config.password
 // );
-
 
 // let client = new cassandra.Client({
 //       contactPoints: [`${config.contactPoint}:${config.port}`],
@@ -28,67 +33,96 @@ const config={username:'cassandra',password:'cassandra',contactPoint:'13.112.103
 //       // },
 //   });
 //   client.connect();
- 
-const { Client } = require("cassandra-driver");
-const cassandrapath ="C:\Users\devik\Downloads\secure-connect-test-cassandra";
-async function run() {
-   const client = new Client({
-      cloud: {
-      secureConnectBundle: cassandrapath,
-      },
-      credentials: {
-      username: config.username,
-      password: config.password,
-      },
-   });
-   await client.connect();
-   // Execute a query
-   const rs = await client.execute("SELECT * FROM system.local");
-   console.log(`Your cluster returned ${rs.rowLength} row(s)`);
-   await client.shutdown();
-}
-// Run the async function
-run();
 
-//cassandra connection end
+//
 
+// const { Client } = require("cassandra-driver");
+// const cassandrapath ="C:\Users\devik\Downloads\secure-connect-test-cassandra";
+// async function run() {
+//    const client = new Client({
+//       cloud: {
+//       secureConnectBundle: cassandrapath,
+//       },
+//       credentials: {
+//       username: config.username,
+//       password: config.password,
+//       },
+//    });
+//    await client.connect();
+//    // Execute a query
+//    const rs = await client.execute("SELECT * FROM system.local");
+//    console.log(`Your cluster returned ${rs.rowLength} row(s)`);
+//    await client.shutdown();
+// }
+// // Run the async function
+// run();
 
+// //cassandra connection end
+
+//npm install cassandra-driver
+let cassandra = require("cassandra-driver");
+let authProvider = new cassandra.auth.PlainTextAuthProvider(
+  "cassandra",
+  "cassandra"
+);
+const keyspace = "test_local";
+let contactPoints = ["localhost"];
+let client = new cassandra.Client({
+  contactPoints: contactPoints,
+  keyspace: keyspace,
+  localDataCenter: "datacenter1"
+});
+client.connect();
+//let query = 'select * from cyclist_byname';
+// let query = "select * from cyclist_byname where firstname=? and lastname=?";
+// //let parameters=[];
+// let parameters = ["Rafael", "Gonçalves"];
+// client.execute(query, parameters, function (error, result) {
+//   if (error != undefined) {
+//     console.log("Error:", err);
+//   } else {
+//     console.table(result.rows);
+//   }
+// });
+let query = "SELECT * FROM system_schema.keyspaces";
+client.execute(query, (err, res) => {
+  if (err != undefined) {
+    console.log("Error:", err);
+  } else {
+    console.table(res.rows);
+  }
+});
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/connect',connectRoute)
-
-
-
-
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/connect", connectRoute);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
